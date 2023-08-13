b0753bdc13dc7350b8b09f2e633673872b3c75ca
xwmtp <xwillmtp@gmail.com>
2023-08-13 20:10:54 +0200

    Add generator versioning

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ npm install --save oot-bingo-lists
 
 ## Usage
 
+### Bingo lists
+
 Use the `getBingoList()` function to retrieve the bingo list of a specific bingo version:
 
 ```ts
@@ -35,28 +37,25 @@ import { getBingoList } from "oot-bingo-lists";
 const bingoList = getBingoList("v10.2");
 ```
 
-### Generator
-
-With
-the [OoT Bingo Generator](https://github.com/ootbingo/oot-bingo-generator) (`npm install --save oot-bingo-generator`),
-these bingo lists can be used to generate boards:
-
-```ts
-import { getBingoList } from "oot-bingo-lists";
-import { generateBingoBoard } from "oot-bingo-generator";
-
-const bingoList = getBingoList("v10.2");
-const board = generateBingoBoard(bingoList, "blackout", 654321);
-```
-
 ### Latest version
 
-Get the latest bingo list:
+Get the bingo list of the latest version:
 
 ```ts
 import { getBingoList, latestBingoVersion } from "oot-bingo-lists";
 
 const bingoList = getBingoList(latestBingoVersion);
+```
+
+### Generate boards
+
+It's also possible to generate boards. The package will use the generator that actually belonged to your
+specified version, so that the resulting boards are correct:
+
+```ts
+import { generateBingoBoardFromVersion } from "oot-bingo-lists";
+
+const board = generateBingoBoardFromVersion("v9.3", "blackout", 654321);
 ```
 
 ### Changelog
@@ -70,8 +69,13 @@ import { printChangeLog, getBingoList } from "oot-bingo-lists";
 printChangeLog(getBingoList("v10.1").normal, getBingoList("v10.2").normal);
 ```
 
-### Sandbox
+## Sandbox
 
 If you're running this project directly instead of importing it in another project, you can run `npm start` to
-execute `src/sandbox/main.ts`. This file contains an example of how to generate a changelog between two versions. Put
-your own proposed bingo list in `exampleBingoList.ts` to see which changes it has compared to an existing version.
+execute `src/sandbox/main.ts`. This file:
+
+* Contains an example of how to generate a changelog between two versions. Put
+  your own proposed bingo list in `exampleBingoList.ts` to see which changes it has compared to an existing version.
+* Demonstrates how to generate a board of a specific version and print the goal names.
+
+Add your own code to this file to access bingo lists, and generate boards and changelogs.
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { BingoList } from "oot-bingo-generator/build/types/goalList";
+import { Mode, Profile } from "oot-bingo-generator/build/types/settings";
+import { BingoBoard } from "oot-bingo-generator/build/bingoBoard";
+import { Generator } from "./types/GeneratorTypes";
+import { Generator_V1 } from "./generators/generator_v1";
 import { bingoList_v9_1 } from "./bingoLists/v9_1";
 import { bingoList_v9_2 } from "./bingoLists/v9_2";
 import { bingoList_v9_3 } from "./bingoLists/v9_3";
@@ -12,21 +16,21 @@ import { bingoList_v10_3 } from "./bingoLists/v10_3";
 import { bingoList_v10_3_1 } from "./bingoLists/v10_3_1";
 
 const bingoListPerVersion = {
-  "v9.1": bingoList_v9_1,
-  "v9.2": bingoList_v9_2,
-  "v9.3": bingoList_v9_3,
-  "v9.4": bingoList_v9_4,
-  "v9.5": bingoList_v9_5,
-  "v9.5.1": bingoList_v9_5_1,
-  "v10.0": bingoList_v10_0,
-  "v10.1": bingoList_v10_1,
-  "v10.2": bingoList_v10_2,
-  "v10.3": bingoList_v10_3,
-  "v10.3.1": bingoList_v10_3_1,
+  "v9.1": { bingoList: bingoList_v9_1, generator: Generator_V1 },
+  "v9.2": { bingoList: bingoList_v9_2, generator: Generator_V1 },
+  "v9.3": { bingoList: bingoList_v9_3, generator: Generator_V1 },
+  "v9.4": { bingoList: bingoList_v9_4, generator: Generator_V1 },
+  "v9.5": { bingoList: bingoList_v9_5, generator: Generator_V1 },
+  "v9.5.1": { bingoList: bingoList_v9_5_1, generator: Generator_V1 },
+  "v10.0": { bingoList: bingoList_v10_0, generator: Generator_V1 },
+  "v10.1": { bingoList: bingoList_v10_1, generator: Generator_V1 },
+  "v10.2": { bingoList: bingoList_v10_2, generator: Generator_V1 },
+  "v10.3": { bingoList: bingoList_v10_3, generator: Generator_V1 },
+  "v10.3.1": { bingoList: bingoList_v10_3_1, generator: Generator_V1 },
 } as const;
 
 export type BingoVersion = keyof typeof bingoListPerVersion;
-export const latestBingoVersion: BingoVersion = "v10.3";
+export const latestBingoVersion: BingoVersion = "v10.3.1";
 
 export const isBingoVersion = (str: string): str is BingoVersion => {
   return str in bingoListPerVersion;
@@ -38,5 +42,33 @@ export const isBingoVersion = (str: string): str is BingoVersion => {
  * @return An object with the (combined) goal list for normal and short bingo
  */
 export function getBingoList(version: BingoVersion): BingoList {
-  return bingoListPerVersion[version];
+  return bingoListPerVersion[version].bingoList;
+}
+
+/**
+ * Get the generator that belongs to a specific bingo version
+ * @param version
+ * @return An object containing the generator functions
+ */
+export function getGenerator(version: BingoVersion): Generator {
+  return bingoListPerVersion[version].generator;
+}
+
+/**
+ * Generate a board of a specific version (using the generator that belonged to that version)
+ * @param version
+ * @param mode
+ * @param seed
+ * @param profile
+ * @return An object of class BingoBoard: contains the squares with the goals, and the number of iterations
+ */
+export function generateBingoBoardFromVersion(
+  version: BingoVersion,
+  mode: Mode,
+  seed: number,
+  profile?: Profile
+): BingoBoard | undefined {
+  const bingoList = getBingoList(version);
+  const generator = getGenerator(version);
+  return generator.generateBingoBoard(bingoList, mode, seed, profile);
 }
